4686f24e1f75465f7e1048d56da18e69ceffd5b9
Sir Cathy W <everbloom@gmail.com>
2025-10-04 14:42:28 +1000

    Add plugin and release script

--- main.ts
+++ main.ts
@@ -1,89 +1,78 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	MarkdownView,
+	Plugin,
+	Menu,
+	TAbstractFile,
+	TFile,
+	PluginSettingTab,
+	App,
+	Setting,
+} from "obsidian";
+import { marked, MarkedOptions } from "marked";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface MdToHtmlSettings {
+	mode: "pedantic" | "gfm" | "default";
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: MdToHtmlSettings = {
+	mode: "default",
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class MdToHtmlPlugin extends Plugin {
+	settings: MdToHtmlSettings;
 
 	async onload() {
 		await this.loadSettings();
+		this.addSettingTab(new MdToHtmlSettingTab(this.app, this));
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (_evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.registerEvent(
+			this.app.workspace.on("editor-menu", (menu, editor, view) => {
+				this.registerMenuItem(menu, async () => {
+					return editor.getValue();
+				}, view instanceof MarkdownView);
+			})
+		);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, _view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+		this.registerEvent(
+			this.app.workspace.on(
+				"file-menu",
+				async (menu, file: TAbstractFile) => {
+					this.registerMenuItem(menu, async () => {
+						return await this.app.vault.cachedRead(file as TFile);
+					}, file instanceof TFile);
 				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+			)
+		);
 	}
 
-	onunload() {
+	onunload() {}
 
+	registerMenuItem(menu: Menu, getMarkdown: () => Promise<string>, enable: boolean) {
+		menu.addItem((item) => {
+			item.setTitle("Copy as HTML")
+				.setIcon("code-xml")
+				.setDisabled(!enable)
+				.onClick(async () => {
+					const markdown = await getMarkdown();
+					const html = await marked.parse(markdown, this.markedOptions);
+					navigator.clipboard.writeText(html);
+				});
+		});
+	}
+
+	get markedOptions(): MarkedOptions {
+		return {
+			pedantic: this.settings.mode === "pedantic",
+			gfm: this.settings.mode === "gfm",
+		};
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
@@ -91,44 +80,31 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+export class MdToHtmlSettingTab extends PluginSettingTab {
+	plugin: MdToHtmlPlugin;
+  
+	constructor(app: App, plugin: MdToHtmlPlugin) {
+	  super(app, plugin);
+	  this.plugin = plugin;
 	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
+  
 	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+	  let { containerEl } = this;
+  
+	  containerEl.empty();
+  
+	  new Setting(containerEl)
+		.setName('Markdown flavour')
+		.addDropdown((dropdown) =>
+			dropdown
+				.addOption('pedantic', 'Pedantic conformance to markdown.pl')
+				.addOption('gfm', 'Github Flavored Markdown')
+				.addOption('default', 'Default')
+				.setValue(this.plugin.settings.mode)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.mode = value as "pedantic" | "gfm" | "default";
 					await this.plugin.saveSettings();
-				}));
+				})
+		);
 	}
-}
+  }
